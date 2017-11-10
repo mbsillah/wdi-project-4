9df6c9869babf0b591703362fd847c4c08170801
sillah2010 <macktastic2014@gmail.com>
2017-11-10 13:44:54 -0500

    Added controllers

---
+++ app/controllers/api/playlists_controller.rb
@@ -0,0 +1,33 @@
+class Api::PlaylistsController < ApplicationController
+    def index
+        @playlists = Playlist.all
+        render json: @playlists
+      end
+    
+      def create
+        @playlist = Playlist.create!(playlist_params)
+        render json: @playlist
+      end
+    
+      def show
+        @playlist = Playlist.find(params[:id])
+        render json: @playlist
+      end
+    
+      def update
+        @playlist = Playlist.find(params[:id])
+        @playlist.update!(playlist_params)
+        render json: @playlist
+      end
+    
+      def destroy
+        @playlist = Playlist.find(params[:id]).delete
+        render status: :ok
+      end
+    
+      private
+    
+      def playlist_params
+        params.require(:playlist).permit(:name)
+      end
+end
---
+++ app/controllers/api/tracks_controller.rb
@@ -0,0 +1,33 @@
+class Api::TracksController < ApplicationController
+    def index
+        @tracks = Track.all
+        render json: @track
+      end
+    
+      def create
+        @track = Track.create!(user_params)
+        render json: @track
+      end
+    
+      def show
+        @track = Track.find(params[:id])
+        render json: @track
+      end
+    
+      def update
+        @track = Track.find(params[:id])
+        @track.update!(user_params)
+        render json: @track
+      end
+    
+      def destroy
+        @track = Track.find(params[:id]).delete
+        render status: :ok
+      end
+    
+      private
+    
+      def track_params
+        params.require(:user).permit(:title, :album, :total_playtime, :release_year)
+      end
+end
